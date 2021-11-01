87528b9a5e646ac023938d2fb02c0535ee89bf1a
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-11-01 22:56:38 +0700

    detach for chorm option

--- test.rb
+++ test.rb
@@ -1,5 +1,15 @@
 require 'watir'
 require 'webdrivers'
-a = Watir::Browser.new:chrome
-a.goto 'https://google.com'
-a.close
+require 'faker'
+require 'selenium'
+
+options = Selenium::WebDriver::Chrome::Options.new
+puts options
+options.add_option(:detach, true)
+browser = Watir::Browser.new :chrome, :options => options
+
+browser.goto 'watir.com/examples/simple_form.html'
+
+browser.text_field(id: 'first_name').set 'Luke'
+browser.text_field(id: 'last_name').set 'Perry'
+# browser.close
