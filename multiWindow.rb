cd5401ba36f6e6048035dc5afdc88d3285524726
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-11-02 12:15:16 +0700

    detach options

--- multiWindow.rb
+++ multiWindow.rb
@@ -12,8 +12,18 @@ browser = Watir::Browser.new :chrome, options: browser_opts
 
 browser.goto 'http://localhost:1000'
 
-button =  browser.a(id: 'test-login')
-puts button.text
-button.click
+# login =  browser.a(id: 'test-login')
+# puts login.text
+# login.click
 
-browser.window(url: 'http://localhost:1000').use
+# browser.window(url: 'http://localhost:1000').use
+
+# register =  browser.a(id: 'test-register')
+# register.click(:control)
+
+browser.a(id: 'test-register').click(:control)
+
+# close child window
+browser.window(url: 'http://localhost:1000').close
+
+
